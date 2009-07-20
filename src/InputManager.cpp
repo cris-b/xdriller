1441d775403c6a4b754b8f9366595c87ef5c2eee
durmieu <durmieu@users.sourceforge.net>
2009-07-20 16:27:56 +0200

    Keyboard and mouse adjustments.

--- src/InputManager.cpp
+++ src/InputManager.cpp
@@ -59,18 +59,18 @@ void InputManager::initialise( Ogre::RenderWindow *renderWindow ) {
 //#endif
 
         //para que no se coma el raton
-        if(XDRILLER_DEBUG == 1)
+        //if(XDRILLER_DEBUG == 1)
         {
             #if defined OIS_WIN32_PLATFORM
-            paramList.insert(std::make_pair(std::string("w32_mouse"), std::string("DISCL_FOREGROUND" )));
-            paramList.insert(std::make_pair(std::string("w32_mouse"), std::string("DISCL_NONEXCLUSIVE")));
-            paramList.insert(std::make_pair(std::string("w32_keyboard"), std::string("DISCL_FOREGROUND")));
-            paramList.insert(std::make_pair(std::string("w32_keyboard"), std::string("DISCL_NONEXCLUSIVE")));
+            //paramList.insert(std::make_pair(std::string("w32_mouse"), std::string("DISCL_FOREGROUND" )));
+            //paramList.insert(std::make_pair(std::string("w32_mouse"), std::string("DISCL_NONEXCLUSIVE")));
+            //paramList.insert(std::make_pair(std::string("w32_keyboard"), std::string("DISCL_FOREGROUND")));
+            //paramList.insert(std::make_pair(std::string("w32_keyboard"), std::string("DISCL_NONEXCLUSIVE")));
             #elif defined OIS_LINUX_PLATFORM
             paramList.insert(std::make_pair(std::string("x11_mouse_grab"), std::string("false")));
-            paramList.insert(std::make_pair(std::string("x11_mouse_hide"), std::string("false")));
+            paramList.insert(std::make_pair(std::string("x11_mouse_hide"), std::string("true")));
             paramList.insert(std::make_pair(std::string("x11_keyboard_grab"), std::string("false")));
-            paramList.insert(std::make_pair(std::string("XAutoRepeatOn"), std::string("true")));
+            paramList.insert(std::make_pair(std::string("XAutoRepeatOn"), std::string("false")));
             #endif
         }
 
